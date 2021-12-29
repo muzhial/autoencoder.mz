d735e9260d95ed6b82b466d9544229df102d7a47
muzhial <kantchenjn@163.com>
2021-12-29 14:27:19 +0000

    add vq_vae

---
+++ ae/models/vq_vae.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class VQEmbeddingEMA(nn.Module):
+    """
+    ref: https://github.com/Jackson-Kang/Pytorch-VAE-tutorial
+    """
+    def __init__(self, n_embeddings, embedding_dim, commitment_cost=0.25, decay=0.999, epsilon=1e-5):
+        super(VQEmbeddingEMA, self).__init__()
+        self.commitment_cost = commitment_cost
+        self.decay = decay
+        self.epsilon = epsilon
+
+        init_bound = 1 / n_embeddings
+        embedding = torch.Tensor(n_embeddings, embedding_dim)
+        embedding.uniform_(-init_bound, init_bound)
+        self.register_buffer("embedding", embedding)
+        self.register_buffer("ema_count", torch.zeros(n_embeddings))
+        self.register_buffer("ema_weight", self.embedding.clone())
+
+    def encode(self, x):
+        M, D = self.embedding.size()
+        x_flat = x.detach().reshape(-1, D)
+
+        distances = torch.addmm(torch.sum(self.embedding ** 2, dim=1) +
+                    torch.sum(x_flat ** 2, dim=1, keepdim=True),
+                                x_flat, self.embedding.t(),
+                                alpha=-2.0, beta=1.0)
+
+        indices = torch.argmin(distances.float(), dim=-1)
+        quantized = F.embedding(indices, self.embedding)
+        quantized = quantized.view_as(x)
+        return quantized, indices.view(x.size(0), x.size(1))
+
+    def retrieve_random_codebook(self, random_indices):
+        quantized = F.embedding(random_indices, self.embedding)
+        quantized = quantized.transpose(1, 3)
+
+        return quantized
+
+    def forward(self, x):
+        M, D = self.embedding.size()
+        x_flat = x.detach().reshape(-1, D)  # (BHW x D)
+
+        distances = torch.addmm(torch.sum(self.embedding ** 2, dim=1) +
+                                torch.sum(x_flat ** 2, dim=1, keepdim=True),
+                                x_flat, self.embedding.t(),
+                                alpha=-2.0, beta=1.0)  # (M, M)
+
+        indices = torch.argmin(distances.float(), dim=-1)  # (M, )
+        encodings = F.one_hot(indices, M).float()  # (M, M)
+        quantized = F.embedding(indices, self.embedding)  # (M, D)
+        quantized = quantized.view_as(x)  # (B, H, W, D)
+
+        if self.training:
+            self.ema_count = self.decay * self.ema_count + (1 - self.decay) * torch.sum(encodings, dim=0)
+            n = torch.sum(self.ema_count)
+            self.ema_count = (self.ema_count + self.epsilon) / (n + M * self.epsilon) * n
+
+            dw = torch.matmul(encodings.t(), x_flat)
+            self.ema_weight = self.decay * self.ema_weight + (1 - self.decay) * dw
+            self.embedding = self.ema_weight / self.ema_count.unsqueeze(-1)
+
+        codebook_loss = F.mse_loss(x.detach(), quantized)
+        e_latent_loss = F.mse_loss(x, quantized.detach())
+        commitment_loss = self.commitment_cost * e_latent_loss
+
+        quantized = x + (quantized - x).detach()
+
+        avg_probs = torch.mean(encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+        return quantized, commitment_loss, codebook_loss, perplexity
